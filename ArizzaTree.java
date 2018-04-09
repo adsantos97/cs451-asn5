dc204b652ec7db5cc94247d134af638a695f467f
adsantos97 <asantos@cs.loyola.edu>
2018-04-09 01:32:28 -0400

    added testcases

--- ArizzaTree.java
+++ ArizzaTree.java
@@ -1,6 +1,9 @@
+// This is my code
+// Arizza Santos
+// CS451 ASN 5
+
 class Node
 {
-  // instance variables
   protected int num;
   protected BinaryTree left, right;
 
@@ -16,11 +19,11 @@ class Node
   public void setNum(int num) { this.num = num; }
   public int getNum() { return num; }
 
-  // setter and getter for left of node
+  // setter and getter for left child of node
   public void setLeftChild(BinaryTree l) { left = l; }
   public BinaryTree getLeftChild() { return left; }
 
-  // setter and getter for right of node
+  // setter and getter for right child of node
   public void setRightChild(BinaryTree r) { right = r; }
   public BinaryTree getRightChild() { return right; }
 
@@ -30,39 +33,32 @@ class BinaryTree
 {
   protected Node root;
 
-  // constructor
-  public BinaryTree()
+  // constructors
+  public BinaryTree() { root = null; }
+  public BinaryTree(Node n) { root = n; }
+
+  public Node getRoot() { return root; }
+
+  // get left subtree
+  public BinaryTree getLeft() 
   { 
-    root = null; 
-  }
-  
-  // parameterized constructor
-  public BinaryTree(Node n)
-  {
-    root = n;
-  }
-
-  public Node getRoot()
-  {
-    return root;
-  }
-
-  public BinaryTree getLeft()
-  {
     BinaryTree t = root.getLeftChild();
     return t;
   }
 
+  // get right subtree
   public BinaryTree getRight()
   {
     BinaryTree t = root.getRightChild();
     return t;
   }
 
-  public boolean emptyTree()
-  {
-    return (root == null);
-  }
+  /*
+   * purpose: checks if tree is empty
+   * input: nothing
+   * returns: true if empty, otherwise false
+   */
+  public boolean emptyTree() { return (root == null); }
 
   /*
    * purpose: insert a number starting from root
@@ -103,6 +99,11 @@ class BinaryTree
     return false;  
   }
 
+  /*
+   * purpose: inorder traversal of the tree
+   * input: nothing
+   * returns: String of inorder traversal
+   */
   public String inorder()
   {
     if (root == null)
@@ -111,6 +112,18 @@ class BinaryTree
       return getLeft().inorder() + root.getNum() + getRight().inorder();
   }
 
+  /*
+   * purpose: prints inorder traversal of the tree
+   * input: nothing
+   * returns: nothing - outputs inorder sequence
+   */
+  public void printInorder() { System.out.println(inorder()); }
+
+  /*
+   * purpose: preorder traversal of the tree
+   * input: nothing
+   * returns: String of preorder traversal
+   */
   public String preorder()
   {
     if (root == null)
@@ -118,6 +131,18 @@ class BinaryTree
     else return " " + root.getNum() + getLeft().preorder() + getRight().preorder();
   }
 
+  /*
+   * purpose: prints preorder traversal of the tree
+   * input: nothing
+   * returns: nothing - outputs preorder sequence
+   */
+  public void printPreorder() { System.out.println(preorder()); }
+
+  /*
+   * purpose: postorder traversal of the tree
+   * input: nothing
+   * returns: String of postorder traversal
+   */
   public String postorder()
   {
     if (root == null)
@@ -126,17 +151,34 @@ class BinaryTree
                 + root.getNum(); 
   }
 
+  /*
+   * purpose: prints postorder traversal of the tree
+   * input: nothing
+   * returns: nothing - outputs postorder sequence
+   */
+  public void printPostorder() { System.out.println(postorder()); }
+
+  /*
+   * purpose: string representation of binary tree object
+   * input: nothing
+   * returns: String - binary tree
+   */
+  public String toString()
+  {
+    if (root == null)
+      return " ";
+    else
+      return root.getNum() + " (" + getLeft().toString() + ") (" 
+             + getRight().toString() + ") "; 
+  }
+
 }
 
 public class ArizzaTree
 { 
-  /**
-   * purpose: test code
-   */
+  //purpose: run code
   public static void main(String[] args)
-  {
-    System.out.println("Binary Tree!");
-    
+  {    
     BinaryTree bt = new BinaryTree();
     
     bt.insert(7);
@@ -150,14 +192,47 @@ public class ArizzaTree
     bt.insert(5);
     bt.insert(4);
     bt.insert(6);
-  
+    
+    System.out.println("Binary Tree");  
+    System.out.println(bt);
+
     System.out.println("Inorder: ");
-    System.out.println(bt.inorder());
+    bt.printInorder();
     System.out.println("Preorder: ");
-    System.out.println(bt.preorder());
+    bt.printPreorder();
     System.out.println("Postorder: ");
-    System.out.println(bt.postorder());
+    bt.printPostorder();
+    System.out.println();
+    
+    /* Tests
 
+      // tree with one number
+      BinaryTree one = new BinaryTree();
+      one.insert(7);
+      System.out.println(one); // 7 () ()
+
+      // tree with one number using a node
+      Node n = new Node(7);
+      BinaryTree oneNode = new BinaryTree(n);
+      System.out.println(oneNode); // 7 () ()
+
+      // tree with left child
+      BinaryTree withLeft = new BinaryTree();
+      withLeft.insert(7);
+      withLeft.insert(6);
+      System.out.println(withLeft); // 7 (6 () () ) ()
+      
+      // tree with right child
+      BinaryTree withRight = new BinaryTree();
+      withRight.insert(7);
+      withRight.insert(6);
+      withRight.insert(8);
+      System.out.println(withRight); // 7 (6 () () ) (8 () () )
+
+      // test traversals
+      withRight.printInorder(); // 6 7 8
+      withRight.printPreorder(); // 7 6 8
+      withRight.printPostorder(); // 6 8 7
+     */
   }
-
 }
